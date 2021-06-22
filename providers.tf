9e464aed97b6012c0d20e2be5993ec5466a37018
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-06-21 22:03:58 -0300

    fix: change ips
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 provider "kubernetes" {
   config_path = "~/.kube/home"
-  host        = "https://192.168.0.134:6443"
+  host        = "https://192.168.68.109:6443"
 }
 
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/home"
-    host        = "https://192.168.0.134:6443"
+    host        = "https://192.168.68.109:6443"
   }
 }
