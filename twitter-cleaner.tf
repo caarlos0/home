2d1ae05b85dc38b17228de9c3acb552288d4509a
Carlos Alexandro Becker <caarlos0@gmail.com>
2021-09-01 22:59:59 -0300

    fix: keep tweet
    Signed-off-by: Carlos Alexandro Becker <caarlos0@gmail.com>

--- twitter-cleaner.tf
+++ twitter-cleaner.tf
@@ -27,7 +27,7 @@ resource "kubernetes_cron_job" "twitter_cleaner" {
             container {
               name  = "twitter-cleaner"
               image = "ghcr.io/caarlos0/twitter-cleaner:latest"
-              args  = ["--max-age=168h", "--debug", "--keep=goreleaser", "--keep=github.com/caarlos0"]
+              args  = ["--max-age=168h", "--debug", "--keep=goreleaser", "--keep=github.com/caarlos0", "--keep=1433157814415941640"]
 
               env_from {
                 secret_ref {
