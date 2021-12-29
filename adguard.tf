605b6990c59eacf45f3872c7c54554252d03fd08
Carlos A Becker <caarlos0@gmail.com>
2021-12-29 14:45:10 -0300

    feat: bump adguard
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- adguard.tf
+++ adguard.tf
@@ -65,7 +65,7 @@ resource "kubernetes_stateful_set" "adguard" {
 
         container {
           name              = "adguard"
-          image             = "adguard/adguardhome:v0.106.3"
+          image             = "adguard/adguardhome:v0.107.1"
           image_pull_policy = "IfNotPresent"
 
           liveness_probe {
